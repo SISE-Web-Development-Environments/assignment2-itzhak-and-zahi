04ff2765ad6430829442b0ec2a2ef4adf3bcdb6b
Tzach <zach.f3@gmail.com>
2020-05-10 02:20:47 +0300

    changing pages

--- keysModel.js
+++ keysModel.js
@@ -8,34 +8,67 @@ function updateKey(event, moving) {
         case "up":
             if(!(event.keyCode===downKey ||event.keyCode===leftKey||event.keyCode===rightKey)) {
                 upKey = event.keyCode;
-                $('#upKeyImg').attr("src", "resources/keys/" + event.keyCode + ".png");
+                if(ImageExist("resources/keys/" + event.keyCode + ".png")) {
+                    $('#upKeyImg').attr("src", "resources/keys/" + event.keyCode + ".png");
+                    $('#inGameKeyUp').attr("src", "resources/keys/" + event.keyCode + ".png");
+                }else {
+                    $('#upKeyImg').attr("src", "resources/keys/0.png");
+                    $('#inGameKeyUp').attr("src", "resources/keys/0.png");
+                }
             }else{
                 window.alert("Please Choose Other Key!");
             }
             break;
-        case "down": downKey = event.which || event.keyCode;
+        case "down": downKey = event.keyCode;
             if(!(event.keyCode===upKey ||event.keyCode===leftKey||event.keyCode===rightKey)) {
-            $('#downKeyImg').attr("src","resources/keys/"+event.keyCode+".png");
+                if(ImageExist("resources/keys/" + event.keyCode + ".png")) {
+                     $('#downKeyImg').attr("src","resources/keys/"+event.keyCode+".png");
+                    $('#inGameKeyDown').attr("src", "resources/keys/" + event.keyCode + ".png");
+                }else {
+                    $('#downKeyImg').attr("src", "resources/keys/0.png");
+                    $('#inGameKeyDown').attr("src", "resources/keys/0.png");
+                }
+
             }else{
                 window.alert("Please Choose Other Key!");
             }
             break;
-        case "left": leftKey = event.which || event.keyCode;
+        case "left": leftKey = event.keyCode;
             if(!(event.keyCode===downKey ||event.keyCode===upKey||event.keyCode===rightKey)) {
+                if(ImageExist("resources/keys/" + event.keyCode + ".png")) {
                 $('#leftKeyImg').attr("src", "resources/keys/" + event.keyCode + ".png");
+                $('#inGameKeyLeft').attr("src", "resources/keys/" + event.keyCode + ".png");
+                }else {
+                    $('#leftKeyImg').attr("src", "resources/keys/0.png");
+                    $('#inGameKeyLeft').attr("src", "resources/keys/0.png");
+                }
+
             }else{
                 window.alert("Please Choose Other Key!");
             }
             break;
-        case "right": rightKey = event.which || event.keyCode;
+        case "right": rightKey =event.keyCode;
             if(!(event.keyCode===downKey ||event.keyCode===leftKey||event.keyCode===upKey)) {
-                $('#rightKeyImg').attr("src", "resources/keys/" + event.keyCode + ".png");
-                $('#inGameKeyRight').attr("src", "resources/keys/" + event.keyCode + ".png");
+                if(ImageExist("resources/keys/" + event.keyCode + ".png")) {
+                    $('#rightKeyImg').attr("src", "resources/keys/" + event.keyCode + ".png");
+                    $('#inGameKeyRight').attr("src", "resources/keys/" + event.keyCode + ".png");
+                }
+                else {
+                        $('#rightKeyImg').attr("src", "resources/keys/0.png");
+                        $('#inGameKeyRight').attr("src", "resources/keys/0.png");
+                    }
             }else{
                 window.alert("Please Choose Other Key!");
             }
             break;
     }
+}
+
+function ImageExist(url)
+{
+    var img = new Image();
+    img.src = url;
+    return img.height != 0;
 }
 
 function GetKeyPressed() {
